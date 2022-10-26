33fc4bf6d38f699d7ea431b4684add5582187b9b
Mattia-Rollo <ste.tia@hotmail.it>
2022-10-26 17:12:48 +0200

    add image click button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 const { createApp } = Vue
 
+
 createApp({
     data() {
         return {
             message: 'hello worldo!',
-            myClass: 'red'
+            myClass: 'red',
+            myImage: ''
         }
     },
     methods: {
-        
+        loadImage() {
+            this.myImage = this.myImage === 'eevee.png' ? '' : 'eevee.png'
+        }
     }
 }).mount('#app')
