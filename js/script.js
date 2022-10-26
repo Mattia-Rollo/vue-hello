8ad45aba21de648cef657bf8ed6fe2604c09ba8e
Mattia-Rollo <ste.tia@hotmail.it>
2022-10-26 18:19:37 +0200

    add random pic

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ createApp({
     methods: {
         loadImage() {
             let numeroRandom = 
-            this.myImage = this.myImage === 'https://lorempokemon.fakerapi.it/pokemon/400/'+Math.floor(Math.random()*100) ? '' : 'https://lorempokemon.fakerapi.it/pokemon/400/'+Math.floor(Math.random()*100);
+            this.myImage = this.myImage === 'https://lorempokemon.fakerapi.it/pokemon/300/'+Math.floor(Math.random()*100) ? '' : 'https://lorempokemon.fakerapi.it/pokemon/300/'+Math.floor(Math.random()*100);
         }
     }
 }).mount('#app')
