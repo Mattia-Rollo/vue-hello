fb4bbcf23eb1afa8efca724562d0374eade4a470
Mattia-Rollo <ste.tia@hotmail.it>
2022-10-26 18:22:32 +0200

    nothing

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ createApp({
         loadImage() {
             let numeroRandom = 
             this.myImage = this.myImage === 'https://lorempokemon.fakerapi.it/pokemon/300/'+Math.floor(Math.random()*100) ? '' : 'https://lorempokemon.fakerapi.it/pokemon/300/'+Math.floor(Math.random()*100);
+            console.log(this.myImage);
         }
     }
 }).mount('#app')
