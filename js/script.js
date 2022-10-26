f62a5817601d77b12162292010bde440a0253345
Mattia-Rollo <ste.tia@hotmail.it>
2022-10-26 14:46:31 +0200

    add script vue

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'hello worldo!',
+            myClass: 'red'
+        }
+    },
+    methods: {
+        
+    }
+}).mount('#app')
